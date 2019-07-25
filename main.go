90717823d07219458f2b7ec9d141de6dbe7f628f
dtmkeng <kang081@hotmail.com>
2019-07-26 06:47:24 +0700

    update render use Echo

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"echo"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/gocolly/colly"
 )
+
+// CourseDetail ...
+type CourseDetail struct {
+	Name  string   `json:"name"`
+	Group int      `json:"group"`
+	Time  []string `json:"time"`
+}
+
+// Course in register this term
+type Course struct {
+	CourseList []string `json:"course_list"`
+}
 
 func main() {
 	c := colly.NewCollector()
 	url := "http://reg5.sut.ac.th/registrar/learn_time.asp?studentid=15917273&f_cmd=2&studentname=%B9%D2%C2+%C0%D9%E4%C1%C2%EC+%A8%D1%B9%B7%C3%EC%C0%D9%A7%D2"
 	var couse []string
 	// Find and visit all links
-
 	c.OnHTML("body > table > tbody > tr:nth-child(1) > td:nth-child(3) > font > b > div > table > tbody > tr > td > table > tbody > tr > td > font > table > tbody > tr > td > table > tbody > tr > td", func(e *colly.HTMLElement) {
 		// e.Request.Visit(e.Attr("href"))
 		// "body > table > tbody > tr:nth-child(1) > td:nth-child(3) > font > b > div > table > tbody > tr > td > table > tbody > tr > td > font > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > font"
@@ -26,12 +40,13 @@ func main() {
 		// for i := 0; i < len(strs); i++ {
 		// 	fmt.Println(strs[i])
 		// }
+
 		e.ForEach("table > tbody > tr > td:nth-child(2) > font", func(_ int, el *colly.HTMLElement) {
 			if el.Text != "ชื่อรายวิชา" {
 				// fmt.Println(el.Text)
 				couse = append(couse, el.Text)
-			}
 
+			}
 		})
 
 	})
@@ -44,5 +59,15 @@ func main() {
 	})
 
 	c.Visit(url)
-	fmt.Println(len(couse))
+	// fmt.Println(len(couse))
+	course := &Course{
+		CourseList: couse,
+	}
+	coursList, _ := json.Marshal(course)
+	fmt.Println(string(coursList))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, course)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
