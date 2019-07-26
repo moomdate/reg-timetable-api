fefbb1fecea4e979d840ae8f0bbb528c0e8c9461
dtmkeng <kang081@hotmail.com>
2019-07-26 07:52:06 +0700

    update and fix bug

--- main.go
+++ main.go
@@ -16,6 +16,16 @@ type CourseDetail struct {
 	Time  []string `json:"time"`
 }
 
+// var ...
+// url ...
+// asd ...
+// var A = "initial"
+const (
+	CourseName = "body > table > tbody > tr:nth-child(1) > td:nth-child(3) > font > b > div > table > tbody > tr > td > table > tbody > tr > td > font > table > tbody > tr > td > table > tbody > tr > td"
+	TermDetail = "body > table > tbody > tr:nth-child(1) > td:nth-child(3) > table:nth-child(3) > tbody > tr:nth-child(7) > td:nth-child(2) > font > font"
+	URL        = "http://reg5.sut.ac.th/registrar/learn_time.asp?studentid=15917273&f_cmd=2&studentname=%B9%D2%C2+%C0%D9%E4%C1%C2%EC+%A8%D1%B9%B7%C3%EC%C0%D9%A7%D2"
+)
+
 // Course in register this term
 type Course struct {
 	CourseList []string `json:"course_list"`
@@ -23,24 +33,11 @@ type Course struct {
 }
 
 func main() {
+	// fmt.Println(A)
 	c := colly.NewCollector()
-	url := "http://reg5.sut.ac.th/registrar/learn_time.asp?studentid=15917273&f_cmd=2&studentname=%B9%D2%C2+%C0%D9%E4%C1%C2%EC+%A8%D1%B9%B7%C3%EC%C0%D9%A7%D2"
 	var couse []string
 	// Find and visit all links
-	c.OnHTML("body > table > tbody > tr:nth-child(1) > td:nth-child(3) > font > b > div > table > tbody > tr > td > table > tbody > tr > td > font > table > tbody > tr > td > table > tbody > tr > td", func(e *colly.HTMLElement) {
-		// e.Request.Visit(e.Attr("href"))
-		// "body > table > tbody > tr:nth-child(1) > td:nth-child(3) > font > b > div > table > tbody > tr > td > table > tbody > tr > td > font > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > font"
-		// with ,_ : =strconv.Atoi()
-		// che := e.ChildText("tr:nth-child(3) > td:nth-child(2) > font")
-		// fmt.Println(che)
-		// str := strings.TrimSpace(e.Text)
-		// fmt.Println(len(str))
-		// strs := strings.Split(str, " ")
-		//
-		// fmt.Println(strs)
-		// for i := 0; i < len(strs); i++ {
-		// 	fmt.Println(strs[i])
-		// }
+	c.OnHTML(CourseName, func(e *colly.HTMLElement) {
 
 		e.ForEach("table > tbody > tr > td:nth-child(2) > font", func(_ int, el *colly.HTMLElement) {
 			if el.Text != "ชื่อรายวิชา" {
@@ -51,12 +48,13 @@ func main() {
 		})
 
 	})
-	c.OnHTML("body > table > tbody > tr:nth-child(1) > td:nth-child(3) > table:nth-child(3) > tbody > tr:nth-child(7) > td:nth-child(2) > font > font", func(e *colly.HTMLElement) {
+	c.OnHTML(TermDetail, func(e *colly.HTMLElement) {
 		// fmt.Println(e.Text)
 		es := strings.Split(e.Text, "/")
 		// es2 := strings.Split(es[1], "/")
 		fmt.Println(es[0]) // acadyear
-		fmt.Println(es[1]) // semester
+		es2 := strings.Split(es[1], "")
+		fmt.Println(es2[1]) // semester
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -66,7 +64,7 @@ func main() {
 		// r.
 	})
 
-	c.Visit(url)
+	c.Visit(URL)
 	courseD := CourseDetail{
 		Name:  "keng",
 		Group: 1,
